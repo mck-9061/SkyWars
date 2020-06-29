c42b646790c4df9ae77a30e4e312a4deafdf4a2d
mck <nathanieladventure@gmail.com>
2020-06-29 17:43:40 +0100

    vscdv

--- src/main/java/me/therealmck/skywars/Main.java
+++ src/main/java/me/therealmck/skywars/Main.java
@@ -1,11 +1,22 @@
 package me.therealmck.skywars;
 
+import me.therealmck.skywars.data.SkyWarsMap;
+import me.therealmck.skywars.placeholderapi.SkyWarsPlaceholderExpansion;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 public class Main extends JavaPlugin {
+    public static List<SkyWarsMap> maps;
+
+
     @Override
     public void onEnable() {
-
+        //PlaceholderAPI
+        if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null){
+            new SkyWarsPlaceholderExpansion(this).register();
+        }
     }
 
     @Override
--- src/main/java/me/therealmck/skywars/data/SkyWarsMap.java
+++ src/main/java/me/therealmck/skywars/data/SkyWarsMap.java
@@ -1,5 +1,6 @@
 package me.therealmck.skywars.data;
 
+import me.therealmck.skywars.Main;
 import org.bukkit.Location;
 import org.bukkit.World;
 
@@ -29,5 +30,14 @@ public class SkyWarsMap {
 
     public void addSpawn(Location spawn) {
         spawns.add(spawn);
+    }
+
+
+    public static SkyWarsMap fromBukkitWorld(World bukkitWorld) {
+        for (SkyWarsMap map : Main.maps) {
+            if (map.bukkitWorld.equals(bukkitWorld)) return map;
+        }
+
+        return null;
     }
 }
